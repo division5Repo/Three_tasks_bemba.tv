27646e6476cd22fb2dc97dbe6427af332d9da231
enri-beezagency <enri@beez.agency>
2022-07-21 22:52:37 +0200

    cleanup

--- public/index.js
+++ public/index.js
@@ -1,85 +1,109 @@
 import * as THREE from 'three'
 import { OrbitControls } from './jsm/controls/OrbitControls.js'
-import { createGeometry, getPositionOfVertex } from './task1Functions.js';
+import { createCubeGeometry, getPositionOfVertex } from './task1Functions.js';
 
-  let container, camera, scene, renderer, mesh;
+let container, camera, scene, renderer, mesh, geometry, spherePositions, lerpValue, indexOfVertex = 0;
 
-  init();
+setupScene();
+loadMeshes();
+setupRenderer();
+setupMeshAnimation();
+activateCameraControls();
+handleClickOnHtmlELements();
+handleWindowResize();
 
-  function init() {
+function setupScene(){
+  
+  container = document.getElementById( 'container' );
 
-    container = document.getElementById( 'container' );
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x8FBCD4 );
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x8FBCD4 );
+  camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 20 );
+  camera.position.z = 10;
 
-    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 20 );
-    camera.position.z = 10;
+  scene.add( camera );
+  scene.add( new THREE.AmbientLight( 0x8FBCD2, 0.35 ) );
 
-    scene.add( camera );
-    scene.add( new THREE.AmbientLight( 0x8FBCD2, 0.35 ) );
+  const pointLight = new THREE.PointLight( 0xffffff, 1 );
+  camera.add( pointLight );
+}
 
-    const pointLight = new THREE.PointLight( 0xffffff, 1 );
-    camera.add( pointLight );
+function loadMeshes(){
 
-    const geometry = createGeometry();
+  const CubeGeometry = createCubeGeometry();
+  geometry = CubeGeometry[0];
+  spherePositions = CubeGeometry[1];
 
-    const material = new THREE.MeshPhongMaterial( {
-      color: 0xff0011,
-      flatShading: true
-    } );
+  const material = new THREE.MeshPhongMaterial( {
+    color: 0xff0011,
+    flatShading: true
+  } );
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+}
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
+function setupRenderer(){
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function setupMeshAnimation(){
+
+  lerpValue = 0.0; 
+  let reverse  = false;
+
+  renderer.setAnimationLoop( function () {
     
-    let inputIndex = document.getElementById('index');
-    let indexOfVertex = 0;
+    if(reverse){
+      lerpValue -= 0.01;
+    }
+    else{
+      lerpValue += 0.01;
+    }
 
-    document.getElementById("btn").addEventListener("click", ()=>{ 
-      
-      if(inputIndex.value !== ''){
-        indexOfVertex = inputIndex.value;
-      }
+    mesh.morphTargetInfluences[0] = lerpValue;
+    mesh.updateMatrixWorld();
+    
+    renderer.render( scene, camera );
+    
+    if(lerpValue > 1.0){
+      reverse = true
+    }
+    else if(lerpValue < 0.0){
+      reverse = false
+    }
+  } );
 
-      getPositionOfVertex(mesh, indexOfVertex)
-    });
+  container.appendChild( renderer.domElement );
+}
 
-    let value = 0.0;
-    let reverse  = false;
-    renderer.setAnimationLoop( function () {
-      
-      if(reverse){
-        value -= 0.001;
-      }
-      else{
-        value += 0.001;
-      }
+function handleClickOnHtmlELements(){
+  
+  let inputIndex = document.getElementById('index');
+  document.getElementById("btn").addEventListener("click", ()=>{ 
 
-      renderer.render( scene, camera );
-      mesh.morphTargetInfluences[ 0 ] = value;
-      
-      if(value > 2.0){
-        reverse = true
-      }
-      else if(value < 0.0){
-        reverse = false
-      }
-    } );
+    if(inputIndex.value !== ''){
+      indexOfVertex = inputIndex.value;
+    }
+    getPositionOfVertex(mesh, indexOfVertex, spherePositions, lerpValue)
+  });
+}
 
-    container.appendChild( renderer.domElement );
+function activateCameraControls(){
 
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.enableZoom = false;
+  const controls = new OrbitControls( camera, renderer.domElement );
+  controls.enableZoom = false;
+}
 
-    window.addEventListener( 'resize', ()=>{
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize( window.innerWidth, window.innerHeight );
-    } );
+function handleWindowResize(){
 
-  }
-
+  window.addEventListener( 'resize', ()=>{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  } );
+}
--- public/task1Functions.js
+++ public/task1Functions.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
+import { Vector3 } from 'three';
 
-export function createGeometry() {
+export function createCubeGeometry() {
 
   const geometry = new THREE.BoxGeometry( 2, 2, 2, 4, 4, 4 );
   geometry.morphAttributes.position = [];
@@ -18,20 +19,26 @@ export function createGeometry() {
       x * Math.sqrt( 1 - ( y * y / 2 ) - ( z * z / 2 ) + ( y * y * z * z / 3 ) ),
       y * Math.sqrt( 1 - ( z * z / 2 ) - ( x * x / 2 ) + ( z * z * x * x / 3 ) ),
       z * Math.sqrt( 1 - ( x * x / 2 ) - ( y * y / 2 ) + ( x * x * y * y / 3 ) )
-
     );
   }
 
   geometry.morphAttributes.position[ 0 ] = new THREE.Float32BufferAttribute( spherePositions, 3 );
-  return geometry;
+  return [geometry, spherePositions];
 }
 
-export function getPositionOfVertex(object, index) {
-
+export function getPositionOfVertex(object, index, spherePositions, lerpValue) {
+  
   const positionAttribute = object.geometry.getAttribute('position');
+  positionAttribute.applyMatrix4( object.matrixWorld );
+  const i = index*3;
 
-  const vertex = new THREE.Vector3();
-  vertex.fromBufferAttribute( positionAttribute, index );
-  console.log('global position of vertex at index: ', index, 'is ', object.localToWorld( vertex ));
-  // alert('global position of vertex at index: ', index, 'is ', object.localToWorld( vertex ));
+  const cubePositionOfVertex = new Vector3(positionAttribute[i], positionAttribute[i+1], positionAttribute[i+2]);
+  cubePositionOfVertex.applyMatrix4( object.matrixWorld ); //global pos of vertex before any animation
+  const spherePositionOfVertex = new Vector3(spherePositions[i], spherePositions[i+1], spherePositions[i+2]);
+  spherePositionOfVertex.applyMatrix4( object.matrixWorld ); //global pos of vertex after animation complete
+
+  let newPosition = new Vector3();
+  newPosition.lerpVectors ( cubePositionOfVertex, spherePositionOfVertex, lerpValue ); //new pos based on lerpValue and 2 positions above
+  
+  console.log('Position of vertex at index: ', index, 'is ', newPosition);
 }
